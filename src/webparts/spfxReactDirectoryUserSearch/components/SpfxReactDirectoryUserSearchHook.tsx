f5593e933da0d37b4e9f905782b51f0e281d15fd
MoonSky1! <vanmathyrr@gmail.com>
2024-04-03 16:47:17 -0400

    Searchbox component included in main component

--- src/webparts/spfxReactDirectoryUserSearch/components/SpfxReactDirectoryUserSearchHook.tsx
+++ src/webparts/spfxReactDirectoryUserSearch/components/SpfxReactDirectoryUserSearchHook.tsx
@@ -1,13 +1,30 @@
 import * as React from 'react';
+import { WebPartTitle } from "@pnp/spfx-controls-react";
 
 import { ISpfxReactDirectoryUserSearchProps } from "./ISpfxReactDirectoryUserSearchProps"
 import styles from './SpfxReactDirectoryUserSearchHook.module.scss';
+import { SearchBox } from 'office-ui-fabric-react';
+import * as strings from 'SpfxReactDirectoryUserSearchWebPartStrings';
 
 
 
 const SpfxReactDirectoryUserSearchHook:React.FC<ISpfxReactDirectoryUserSearchProps> = (props)=>{
 return (
   <div className={styles.directory}>
+    <WebPartTitle
+      displayMode={props.displayMode}
+      title={props.title}
+      updateProperty={props.updateProperty}
+    />
+    <div className={styles.searchBox}>
+      <SearchBox
+        placeholder={strings.SearchPlaceHolder}
+        className={styles.searchTextBox}
+        onSearch={_searchUsers}
+        value={state.searchText}
+        onChange={(ev, newVal) => _searchBoxChanged(newVal)}
+      />
+    </div>
     <h1>Hello</h1>
   </div>
 );
